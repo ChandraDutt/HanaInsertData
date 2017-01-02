9c4e14581ccd1b2793f02542cb7e0a0edab238b9
ChandraDutt <ChandraDutt@192.168.0.9>
2017-01-03 00:33:13 +0530

    Supplier data is done

---
+++ Data.py
@@ -0,0 +1,24 @@
+'''
+Created on 02-Jan-2017
+
+@author: ChandraDutt
+'''
+Cities = ['Vizag','Hyderabad','Vijayawada','Guntur','Mumbai','Pune','Bangalore','Mysore','Bhuvaneswar','Kolkata','Chennai','Pondicherry','Trivandrum','New Delhi']
+
+Suppliers = {'TATA Motors Ltd':'Tatamotors@tataltd.com',
+             'Mahindra Motors Ltd':'Motors@mahindra.com',
+             'Indian Pictures Ltd':'IndianPictures@gmail.com',
+             'Signoraware Plastics':'SignorawarePlastics@rediff.com',
+             'Axis Bank':'Axisbank@axispvtbnk.com',
+             'HCL Technologies':'Motors@mahindra.com',
+             'Walmart':'walmart@gmail.com',
+             'Exxon Mobil':'exxonmobil@yahoo.in',
+             'Apple Inc.':'AppleInc@apple.com',
+             'Berkshire Hathway':'Berkshire@hathway.com',
+             'United Health Group':'Unitedhealth@uhg.com',
+             'CVS Health':'cvshealth@csv.com',
+             'General Motors':'generalmotors@gm.com',
+             'Ford Motor':'fordmotors@ford.com',
+             'AT&T Inc':'at&t@at&tinc.com',
+             'General Electric':'generalelectric@ge.com'
+             }
--- SupplierData.py
+++ SupplierData.py
@@ -1,2 +1,122 @@
-SuppReq = int(input('Enter required number of suppliers'));
-print("Required number of suppliers are",SuppReq);
+import random;
+import Data
+# SuppReq = int(input('Enter required number of suppliers'));
+# print("Required number of suppliers are",SuppReq);
+
+class Supplier:
+    supplierId = 1
+    def __init__(self,SupplierId,SupplierName,EmailId,StartDate,CityName):
+        self.SupplierId = Supplier.supplierId
+        self.SupplierName = SupplierName
+        self.EmailId = EmailId
+        self.StartDate = StartDate
+        self.CityName = CityName
+        Supplier.supplierId += 1
+
+    def get_supplier_count(self):
+        return self.__SupplierCount
+
+
+    def get_supplier_id(self):
+        return self.__SupplierId
+
+
+    def get_supplier_name(self):
+        return self.__SupplierName
+
+
+    def get_email_id(self):
+        return self.__EmailId
+
+
+    def get_start_date(self):
+        return self.__StartDate
+
+
+    def get_city_name(self):
+        return self.__CityName
+
+
+    def set_supplier_count(self, value):
+        self.__SupplierCount = value
+
+
+    def set_supplier_id(self, value):
+        self.__SupplierId = value
+
+
+    def set_supplier_name(self, value):
+        self.__SupplierName = value
+
+
+    def set_email_id(self, value):
+        self.__EmailId = value
+
+
+    def set_start_date(self, value):
+        self.__StartDate = value
+
+
+    def set_city_name(self, value):
+        self.__CityName = value
+
+
+    def del_supplier_count(self):
+        del self.__SupplierCount
+
+
+    def del_supplier_id(self):
+        del self.__SupplierId
+
+
+    def del_supplier_name(self):
+        del self.__SupplierName
+
+
+    def del_email_id(self):
+        del self.__EmailId
+
+
+    def del_start_date(self):
+        del self.__StartDate
+
+
+    def del_city_name(self):
+        del self.__CityName
+
+
+    SupplierCount = property(get_supplier_count, set_supplier_count, del_supplier_count, "SupplierCount's docstring")
+    SupplierId = property(get_supplier_id, set_supplier_id, del_supplier_id, "SupplierId's docstring")
+    SupplierName = property(get_supplier_name, set_supplier_name, del_supplier_name, "SupplierName's docstring")
+    EmailId = property(get_email_id, set_email_id, del_email_id, "EmailId's docstring")
+    StartDate = property(get_start_date, set_start_date, del_start_date, "StartDate's docstring")
+    CityName = property(get_city_name, set_city_name, del_city_name, "CityName's docstring")
+    
+    
+def generateRandomCity(inputVar):
+    return(random.choice(inputVar));
+
+def generateRandomDate():
+    year = random.randint(1990,2016);
+    month = random.randint(1,12);
+    if month == 2:
+        l_year = isLeapYear(year);
+        if l_year == True:
+            date = random.randint(1,29);
+        else:
+            date = random.randint(1,28)
+    elif month in (4,6,9,11) :
+        date = random.randint(1,30);
+    else:
+        date = random.randint(1,31);
+    return (str(year)+'-'+str(month)+'-'+str(date));
+def isLeapYear(year):
+    if ((year%4 == 0) & (year%100 != 0)) | (year%400 == 0):
+        return True
+    else:
+        return False
+
+
+for key,value in Data.Suppliers.items():
+    supplierObj = Supplier('',key,value,generateRandomDate(),generateRandomCity(Data.Cities))
+    print(supplierObj.get_supplier_id(),supplierObj.get_supplier_name(),supplierObj.get_start_date(),supplierObj.get_city_name());
